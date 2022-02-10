a94585db3852ce40819816a922c449f1087333de
cs <xxxx@example.com>
2022-02-10 14:08:50 +0900

    追加

--- todoapp/app2.js
+++ todoapp/app2.js
@@ -8,11 +8,33 @@ Vue.createApp({
             hideDoneTodo: false,
             searchWord: '',
             order: 'desc',
+            categoryName: '',
         }
     },
     computed: {
         canCreateTodo: function() {
             return this.todoTitle !== ''
+        },
+        canCreateCategory: function() {
+            return this.categoryName !== ''
+        },
+    },
+    methods: {
+        createTodo: function() {
+            if(!this.canCreateTodo) {
+                return
+            }
+
+            this.todoTitle = ''
+            this.todoDescription = ''
+            this.todoCategories = []
+        },
+        createCategory: function() {
+            if(!this.canCreateCategory) {
+                return
+            }
+
+            this.categoryName = ''
         },
     },
 }).mount('#app')
