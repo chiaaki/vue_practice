10c340edb99dc496e68fde4f1d1d5bbe2a8b6fdc
cs <xxxx@example.com>
2022-03-09 15:00:06 +0900

    feat：v-forの追加

--- practice/app.js
+++ practice/app.js
@@ -1,6 +1,29 @@
 Vue.createApp({
     data: function() {
         return {
+            items2: [
+                {
+                    id: 1,
+                    name: 'item-1',
+                },
+                {
+                    id: 2,
+                    name: 'item-2',
+                },
+            ],
+
+            object: {
+                name: 'ヤマダ',
+                age: 40,
+                gender: '女',
+            },
+
+            items: ['item-1', 'item-2'],
+
+            value: 7,
+
+            isShow: false,
+
             defaultColor: 'blue',
 
             isActive: false,
